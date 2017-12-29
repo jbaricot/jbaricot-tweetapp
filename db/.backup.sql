e0b791a29bc78608363fd9f195026e96b0748a8f
Jonathan Baricot <jbaricot@gmail.com>
2017-12-28 19:48:08 -0500

    Update files

---
+++ db/.backup.sql
@@ -0,0 +1,14 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "schema_migrations" ("version" varchar NOT NULL PRIMARY KEY);
+INSERT INTO schema_migrations VALUES('20170331045923');
+CREATE TABLE IF NOT EXISTS "ar_internal_metadata" ("key" varchar NOT NULL PRIMARY KEY, "value" varchar, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO ar_internal_metadata VALUES('environment','development','2017-03-31 05:21:00.324397','2017-03-31 05:21:00.324397');
+CREATE TABLE IF NOT EXISTS "posts" ("id" INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, "content" text, "created_at" datetime NOT NULL, "updated_at" datetime NOT NULL);
+INSERT INTO posts VALUES(1,'Looking for a good book to read.','2017-03-31 05:24:15.529063','2017-03-31 05:24:15.529063');
+INSERT INTO posts VALUES(2,'Today''s lunch was great!','2017-03-31 05:24:32.004378','2017-03-31 05:24:32.004378');
+INSERT INTO posts VALUES(4,'I think it''s time.....','2017-12-28 17:53:55.340675','2017-12-28 18:07:42.996960');
+INSERT INTO posts VALUES(6,replace(replace('"Post successfully deleted"\r\n\r\n2017','\r',char(13)),'\n',char(10)),'2017-12-28 21:04:06.777263','2017-12-28 21:04:20.843737');
+DELETE FROM sqlite_sequence;
+INSERT INTO sqlite_sequence VALUES('posts',6);
+COMMIT;
